7fc3e4ab7507f99737f631b745c75d7730590db7
Laércio Vitorino <laerciovitorino@gmail.com>
2019-05-23 22:11:56 -0300

    Update code to use local storage as a first way for the app to retain data

--- src/app.js
+++ src/app.js
@@ -11,11 +11,23 @@ class IndecisionApp extends React.Component {
   }
 
   componentDidMount() {
-    console.log('componentDidMonut!');
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      // Do nothing at all  
+    }
   }
 
-  componentDidUpdate() {
-    console.log('componentDidUpdate!');
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
   }
 
   componentWillUnmount() {
@@ -108,6 +120,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started!</p>}
       {
         props.options.map((option) => (
           <Option
@@ -150,9 +163,12 @@ class AddOption extends React.Component {
     
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
-    e.target.elements.option.value = '';
 
     this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
   }
 
   render() {
